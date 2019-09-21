5b1c45ae27c0bf451aaf2a25a5e52dc0171ac801
Ted Wang <ted.wang2012@gmail.com>
2019-09-22 06:47:51 +0800

    fix a wrong command

--- env_init.sh
+++ env_init.sh
@@ -8,6 +8,6 @@ cat ./bash/.bashrc >> ~/.bashrc
 
 echo correct font fall back
 sudo cp /etc/fonts/conf.avail/64-language-selector-prefer.conf /etc/fonts/conf.avail/64-language-selector-prefer.conf.bak
-sudo cat ./linux/64-language-selector-prefer.conf > /etc/fonts/conf.avail/64-language-selector-prefer.conf
+sudo sh -c 'sudo cat ./linux/64-language-selector-prefer.conf > /etc/fonts/conf.avail/64-language-selector-prefer.conf'
 sudo fc-cache -fv
 
