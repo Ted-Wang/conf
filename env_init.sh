c16f5c7a4a2e41fa9030265f3b3154e395e68fa7
Ted Wang <ted.wang2012@gmail.com>
2019-11-18 07:12:01 +0800

    update shell script as file renamed.

--- env_init.sh
+++ env_init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo init .vimrc
-cat ./vim/.vimrc >> ~/.vimrc
+cat ./vim/ted.vimrc >> ~/.vimrc
 
 echo init .bashrc
-cat ./bash/.bashrc >> ~/.bashrc
+cat ./bash/ted.bashrc >> ~/.bashrc
 
 echo correct font fall back
 sudo cp /etc/fonts/conf.avail/64-language-selector-prefer.conf /etc/fonts/conf.avail/64-language-selector-prefer.conf.bak
