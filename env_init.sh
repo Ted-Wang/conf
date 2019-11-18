22301a4a07a9ec2f67654e95b505873f57b128ad
Ted Wang <ted.wang2012@gmail.com>
2019-11-19 07:31:12 +0800

    update env_init.sh

--- env_init.sh
+++ env_init.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-echo init .vimrc
-cat ./vim/ted.vimrc >> ~/.vimrc
+echo setting up .vimrc
+cp vim/ted.vimrc ~/
+sudo chmod 644 ~/ted.vimrc
+if [ ! -f ~/.vimrc ]; then
+    touch ~/.vimrc
+    sudo chmod 644 ~/.vimrc
+fi
+sudo echo "so ~/ted.vimrc" >> ~/.vimrc
 
-echo init .bashrc
-cat ./bash/ted.bashrc >> ~/.bashrc
+echo setting up .bashrc
+cp bash/ted.bashrc ~/
+sudo chmod 644 ~/ted.bashrc
+if [ ! -f ~/.bashrc ]; then
+    touch ~/.bashrc
+    sudo chmod 644 ~/.bashrc
+fi
+sudo echo "so ~/ted.bashrc" >> ~/.bashrc
 
 echo correct font fall back
 sudo cp /etc/fonts/conf.avail/64-language-selector-prefer.conf /etc/fonts/conf.avail/64-language-selector-prefer.conf.bak
