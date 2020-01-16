d28f95541d2c50a141278b65b32af0284c5b1260
Ted Wang <ted.wang2012@gmail.com>
2020-01-16 11:01:12 +0800

    update git-prompt.sh for Manjaro.

--- bash/git-prompt.sh
+++ bash/git-prompt.sh
@@ -1,3 +1,9 @@
 
+# import __git_ps1() function for Manjaro
+pacman --help > /dev/null
+if [ $? == 0 ] && [ -f /usr/share/git/completion/git-prompt.sh ]; then
+    source /usr/share/git/completion/git-prompt.sh
+fi
+
 export PS1="${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[36m\]\$(__git_ps1 '(%s)')\[\033[0m\]\$ "
 
