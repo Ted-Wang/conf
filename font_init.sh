b2a679025ae5fb4c788c8d24c21bd6d9d7342a98
Ted Wang <ted.wang2012@gmail.com>
2020-02-17 18:31:30 +0800

    use relative BASE path in font_init.sh

--- font_init.sh
+++ font_init.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # When Linux OS installed with English as default language, the Chinese character may be displayed incorrect due to the wrong font fallback priority.
 echo correct font fall back
-sudo cp /etc/fonts/conf.avail/64-language-selector-prefer.conf /etc/fonts/conf.avail/64-language-selector-prefer.conf.bak
-sudo sh -c 'sudo cat ./linux_conf/64-language-selector-prefer.conf > /etc/fonts/conf.avail/64-language-selector-prefer.conf'
+sudo \cp /etc/fonts/conf.avail/64-language-selector-prefer.conf /etc/fonts/conf.avail/64-language-selector-prefer.conf.bak
+sudo sh -c 'sudo cat $BASE_DIR/linux_conf/64-language-selector-prefer.conf > /etc/fonts/conf.avail/64-language-selector-prefer.conf'
 sudo fc-cache -fv
