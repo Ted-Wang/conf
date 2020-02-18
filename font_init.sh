66ef42a249cc037accf58b4157922e707df32e89
Ted Wang <ted.wang2012@gmail.com>
2020-02-18 22:41:45 +0800

    update file font_init.sh

--- font_init.sh
+++ font_init.sh
@@ -6,4 +6,9 @@ BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo correct font fall back
 sudo \cp /etc/fonts/conf.avail/64-language-selector-prefer.conf /etc/fonts/conf.avail/64-language-selector-prefer.conf.bak
 sudo sh -c 'sudo cat $BASE_DIR/linux_conf/64-language-selector-prefer.conf > /etc/fonts/conf.avail/64-language-selector-prefer.conf'
+pamcan --help > /dev/null 2>&1
+if [ $? == 0 ]; then
+    # create symbol link for Manjaro
+    sudo ln -s /etc/fonts/conf.avail/64-language-selector-prefer.conf /etc/fonts/conf.d/64-language-selector-prefer.conf
+fi
 sudo fc-cache -fv
