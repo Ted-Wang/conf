302afec9c55a76ef801daa96e9b42658fa1c8e5e
Ted Wang <ted.wang2012@gmail.com>
2021-11-03 19:09:22 +0800

    fix an syntax fault in env_init.sh

--- env_init.sh
+++ env_init.sh
@@ -33,7 +33,7 @@ if [[ "$runIn" == "Mac" ]];then
     #BASE_DIR=${0%/*}
     BASE_DIR=$(dirname ${BASH_SOURCE[0]-$0})
 else
-    BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[[0]]}" )" >/dev/null 2>&1 && pwd )"
+    BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 fi
 echo $BASE_DIR
 
