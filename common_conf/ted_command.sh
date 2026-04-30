47d4182a026c5325b8b61d49b8004316f16e22a0
Ted Wang <ted.wang2012@gmail.com>
2026-04-30 09:07:58 +0800

    add command completion for showip and showip6

--- common_conf/ted_command.sh
+++ common_conf/ted_command.sh
@@ -5,8 +5,8 @@
 #alias showip6="ip -o -f inet6 addr show | awk '/scope global/ {print \$2, \$4}'"
 
 function showip() {
-    fam="inet"
-    dev=""
+    local fam="inet"
+    local dev=""
     if [ ! -z $1 ]; then
         dev="dev $1"
     fi
@@ -14,11 +14,24 @@ function showip() {
 }
 
 function showip6() {
-    fam="inet6"
-    dev=""
+    local fam="inet6"
+    local dev=""
     if [ ! -z $1 ]; then
         dev="dev $1"
     fi
     ip -o -f $fam addr show $dev | awk '/scope global/ {print $2, $4}'
 }
 
+if [ -n "BASH_VERSION" ]; then
+    function _showip_complete() {
+        local curword="${COMP_WORDS[COMP_CWORD]}"
+        local opts=$(ip -o -f inet addr show | awk '/scope global/ {print $2}')
+        COMPREPLY=( $(compgen -W "${opts}" -- "${curword}") )
+        #if [[ "$curword" =~ .+ ]]; then
+            #COMPREPLY=($(echo $opts | sed -n "/^${curword}.*/p"))
+        #else
+            #COMPREPLY=($opts)
+        #fi
+    }
+    complete -F _showip_complete showip
+fi
